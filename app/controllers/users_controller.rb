6688fe7bcb421d131db96645f3a1c8f3d67ea525
EC2 Default User <ec2-user@ip-172-31-33-190.ap-northeast-1.compute.internal>
2022-09-30 04:28:29 +0000

    bookers2[bootstrap導入]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
-
+  
+  def index
+  end
+  
+  
   def edit
     @user = User.find(params[:id])
   end
